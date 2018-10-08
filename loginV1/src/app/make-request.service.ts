73b5354fb985a1c709c942f9283094c9bd4c8bbb
Sai Kumar <saikumara@wavelabs.in>
2018-10-08 16:43:13 +0530

    Adding Constant variables

--- loginV1/src/app/make-request.service.ts
+++ loginV1/src/app/make-request.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, retry, catchError } from 'rxjs/operators';
+import { config } from './utils/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -11,30 +12,30 @@ export class MakeRequestService {
   constructor(private http: HttpClient) { }
   //POST
   post(url: string, data: any): Observable<any> {
-    return this.http.post(url, {}, data).pipe(map((response: any) => this.handleResponse(response)),
+    return this.http.post(`${config['BASE_URL']}${url}`, {}, data).pipe(map((response: any) => this.handleResponse(response)),
       // if error occure try 2 time
       catchError(() => of(retry(2)))
     )
   }
   //GET
-  get(url:string,dataParams:any):Observable<any>{
-    return this.http.get(url,{}).pipe(map((response:any)=> this.handleResponse(response))
+  get(url: string, dataParams: any): Observable<any> {
+    return this.http.get(`${config['BASE_URL']}${url}`, {}).pipe(map((response: any) => this.handleResponse(response)))
   }
   //PUT
   put(url: string, data: any): Observable<any> {
-    return this.http.put(url, {}, data).pipe(map((response: any) => this.handleResponse(response)),
+    return this.http.put(`${config['BASE_URL']}${url}`, {}, data).pipe(map((response: any) => this.handleResponse(response)),
       // if error occure try 2 time
       catchError(() => of(retry(2)))
     )
   }
   //DELETE
   delete(url: string, data: any): Observable<any> {
-    return this.http.delete(url, {}).pipe(map((response: any) => this.handleResponse(response)),
+    return this.http.delete(`${config['BASE_URL']}${url}`, {}).pipe(map((response: any) => this.handleResponse(response)),
       // if error occure try 2 time
       catchError(() => of(retry(2)))
     )
   }
-  handleResponse(response){
+  handleResponse(response) {
     if (response.status === 403) {
       return 'unauthorized';
     } else {
---
+++ loginV1/src/app/utils/constants.ts
@@ -0,0 +1,3 @@
+export const config: object = Object.freeze({
+    "BASE_URL": "http://localhost:3000/user"
+})
