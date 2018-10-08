de7e0cef2914316003b16621944e4093b3ac57bd
Sai Kumar <saikumara@wavelabs.in>
2018-10-08 14:24:39 +0530

    routing module added using ng generate module app-routing --flat --module=app

--- loginV1/src/app/app.module.ts
+++ loginV1/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
