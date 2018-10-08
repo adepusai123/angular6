d369321aa436cd29963c87a8200ac094b5a2f12f
Sai Kumar <saikumara@wavelabs.in>
2018-10-08 14:52:44 +0530

    Routing done with configuration in routes.ts file

--- loginV1/src/app/app-routing.module.ts
+++ loginV1/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full'},
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
+  exports: [RouterModule],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
   declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  title = "Sample User Authentication"
+}
