fd6b465c36ab76858f53826d936197dcc601b144
Sai Kumar <saikumara@wavelabs.in>
2018-10-08 16:06:49 +0530

    Login Form validation Done using FormBuilder & Reactive Forms

--- loginV1/src/app/app.module.ts
+++ loginV1/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +21,9 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
