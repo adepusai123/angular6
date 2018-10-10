d6ac4bf091aca4ab7328319a91e184e61562187f
Sai Kumar <saikumara@wavelabs.in>
2018-10-10 12:30:33 +0530

    protected dashboard route with AuthGuard

--- loginV1/src/app/app-routing.module.ts
+++ loginV1/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full'},
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent,canActivate:[AuthGuard] },
 ];
 
 @NgModule({
